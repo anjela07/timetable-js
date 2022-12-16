cfeeba220e612ed1d53c6e52c5573a7f89f90bf2
Angela <116427072+anjela07@users.noreply.github.com>
2022-12-16 14:50:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+let body = document.body
+let write  = document.createElement('h1')
+write.classList='text'
+write.innerHTML='Timetable'
+body.append(write)
+let main=document.createElement('div')
+main.classList ='main'
+body.append(main)
+let sq1 = document.createElement('div')
+sq1.classList="sq"
+main.append(sq1)
+let tex1 = document.createElement('nav')
+tex1.classList="tex"
+tex1.innerHTML='<span>8:00</span>'
+sq1.append(tex1)
+let pic = document.createElement('div')
+pic.classList = "pic1"
+pic.innerHTML= '<img class = "xxx" src="./math.png">'
+sq1.append(pic)
+let sq2 = document.createElement('div')
+sq2.classList='sq'
+main.append(sq2)
+let tex2 = document.createElement('div')
+tex2.classList='tex'
+tex2.innerHTML='<span>8:45</span>'
+sq2.append(tex2)
+let pic2 = document.createElement('div')
+pic2.classList='pic1'
+pic2.innerHTML= '<img class="xxx" src="./bell.png">'
+sq2.append(pic2)
+let sq3 = document.createElement('div')
+sq3.classList='sq3'
+main.append(sq3)
+let tex3 = document.createElement('nav')
+tex3.classList='tex'
+tex3.innerHTML='<span>8:50</span>'
+sq3.append(tex3)
+let pic3 = document.createElement('div')
+pic3.classList='pic3'
+pic3.innerHTML='<img class="xxx" src="./english.png">'
+sq3.append(pic3)
+let sq4 = document.createElement('div')
+sq4.classList='sq4'
+main.append(sq4)
+let tex4 = document.createElement('div')
+tex4.classList='tex'
+tex4.innerHTML='<span>9:35</span>'
+sq4.append(tex4)
+let pic4 = document.createElement('div')
+pic4.classList='pic3'
+pic4.innerHTML='<img class="xxx" src="./bell.png">'
+sq4.append(pic4)
+let sq5 = document.createElement('div')
+sq5.classList='sq5'
+main.append(sq5)
+let tex5 = document.createElement('nav')
+tex5.classList='tex'
+tex5.innerHTML='<span>9:40</span>'
+sq5.append(tex5)
+let pic5 = document.createElement('div')
+pic5.classList='pic3'
+pic5.innerHTML='<img class="xxx" src="./physical.png">'
+sq5.append(pic5)
+let sq6 = document.createElement('div')
+sq6.classList='sq5'
+main.append(sq6)
+let tex6 = document.createElement('nav')
+tex6.classList='tex'
+tex6.innerHTML='<span>10:25</span>'
+sq6.append(tex6)
+let pic6 = document.createElement('div')
+pic6.classList='pic3'
+pic6.innerHTML='<img class="xxx" src="./bell.png">'
+sq6.append(pic6)
+
+let sq7 = document.createElement('div')
+sq7.classList='sq7'
+main.append(sq7)
+let tex7 = document.createElement('nav')
+tex7.classList='tex'
+tex7.innerHTML='<span>10:30</span>'
+sq7.append(tex7)
+let pic7 = document.createElement('div')
+pic7.classList='pic3'
+pic7.innerHTML='<img class="xxx" src="./arts.png">'
+sq7.append(pic7)
+
+let sq8 = document.createElement('div')
+sq8.classList='sq7'
+main.append(sq8)
+let tex8 = document.createElement('nav')
+tex8.classList='tex'
+tex8.innerHTML='<span>10:30</span>'
+sq8.append(tex8)
+let pic8 = document.createElement('div')
+pic8.classList='pic3'
+pic8.innerHTML='<img class="xxx" src="./bell.png">'
+sq8.append(pic8)
+
+let sq9 = document.createElement('div')
+sq9.classList='sq5'
+main.append(sq9)
+let tex9 = document.createElement('nav')
+tex9.classList='tex'
+tex9.innerHTML='<span>11:15</span>'
+sq9.append(tex9)
+let pic9 = document.createElement('div')
+pic9.classList='pic3'
+pic9.innerHTML='<img class="xxx" src="./home.png">'
+sq9.append(pic9)
+
+
+
+
+
